3cf95048152281fa5ead8a09cd0fdf56eda44736
lsteiner9 <lsteiner1@cnm.edu>
2021-03-19 22:05:13 -0600

    Working sort.

--- bubblesort.py
+++ bubblesort.py
@@ -1,5 +1,24 @@
 def sort(nums):
-    for i in range (0, len(nums)):
-        for j in range (0, len(nums) - 2):
-            if nums[j] > nums [j + 1]:
-                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+    length = len(nums)
+    while True:
+        count = 0
+        for i in range(0, length - 1):
+            if nums[i] > nums[i + 1]:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                count += 1
+        if count == 0:
+            break
+
+
+def main():
+    length = int(input("First enter the number of elements, then enter each "
+                       "element individually:  "))
+    nums = []
+    for i in range(0, length):
+        nums.append(int(input()))
+    sort(nums)
+    print(nums)
+
+
+if __name__ == '__main__':
+    main()
